788e9009d8f8dfb07d6e755ae0e9e37580523b14
chi-hung <chi-hung.weng@gmx.de>
2017-10-16 11:20:59 +0800

    fix CNTK support.

--- ndrun.sh
+++ ndrun.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # This is a wrapper file that helps you to run your script within our container.
+
 # Examples of Usage (print Tensorflow's version within the container):
 #   printf "import tensorflow \nprint('tensorflow version=',tensorflow.__version__)" > check_tf_version.py
 #   ndrun -t tf-cu9-dnn7-py3 python3 check_tf_version.py 
@@ -24,7 +25,7 @@ HOST_VOL="$(dirname "$EXE")"
 
 # If the path is not given explicitly,
 # then the path is the current working directory.
-if [ "$HOST_VOL" == '.' ] ; then
+if [ "$HOST_VOL" == '.' ] ;then
   HOST_VOL=$PWD
 fi
 
@@ -36,12 +37,24 @@ WORKSPACE=/notebook
 
 # print the command that is going to be executed within the container.
 echo "${INTEPRETER} ${WORKSPACE}/${EXE}"
-
 # Command to initialize a temporary docker container
-# in order to run the script. 
-nvidia-docker run \
--it \
--v ${HOST_VOL}:${WORKSPACE} \
---rm \
-honghu/keras:${IMG_TAG} \
-${INTEPRETER} ${WORKSPACE}/${EXE}
+# in order to run the script.
+
+# In order to use CNTK containers, we'll have to
+# switch on the CNTK's virtual environment.
+if [ "${IMG_TAG}" == "cntk-cu8-dnn6-py3" ] ;then
+  SRC_CNTK="source /cntk/activate-cntk"
+  nvidia-docker run \
+  -it \
+  -v ${HOST_VOL}:${WORKSPACE} \
+  --rm \
+  honghu/keras:${IMG_TAG} \
+  "${SRC_CNTK} && ${INTEPRETER} ${WORKSPACE}/${EXE}"
+else
+  nvidia-docker run \
+  -it \
+  -v ${HOST_VOL}:${WORKSPACE} \
+  --rm \
+  honghu/keras:${IMG_TAG} \
+  ${INTEPRETER} ${WORKSPACE}/${EXE}
+fi
