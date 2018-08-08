cecfaa9032edd2928270d73085e0daa53dce60f6
chi-hung <chi-hung.weng@gmx.de>
2018-08-08 13:08:09 +0800

    ndrun.sh is improved.

--- ndrun.sh
+++ ndrun.sh
@@ -1,4 +1,6 @@
-# Copyright 2017 Chi-Hung Weng
+###########################################################################################################
+#
+# Copyright 2018 Chi-Hung Weng
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,50 +13,171 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
+##########################################################################################################
+#
+# NAME
+#      ndrun - activate and use different docker images for deep learning research
+#
+# SYNOPSIS
+#      ndrun [-v] [-h] [-t IMG_TAG/IMG_TYPE] [-n NUM_GPUS] [-a ALIAS] [--extra EXTRA_OPTS]
+#
+# COMMAND-LINE OPTIONS
+#
+#      -t [IMG_TAG/IMG_TYPE]
+#      --tag [IMG_TAG]
+#      --type [IMG_TYPE]
+#            The image to be activated is chosen according to the given image's tag, [IMG_TAG]
+#            or type, [IMG_TYPE].
+#
+#            The available tags can be found at:
+#                  https://hub.docker.com/r/honghu/keras/tags/
+#
+#            The possible values of [IMG_TYPE] are:
+#                  tensorflow, cntk, mxnet, theano
+#
+#            If you provide the type of image, then, the latest image of
+#            the specified framework will be activated.
+#
+#            Remark:
+#                  * the defult image's name is TensorFlow
+#                  * the chosen image must belong to the repository: honghu/keras
+# 
+#      -n, --num_gpus [NUM_GPUS]
+#            Set [NUM_GPUS], which is the number of GPUs to be used by the running image.
+#
+#            GPUs that are less busy will be selected.
+# 
+#            [NUM_GPUS] is set as 1 by default (single GPU mode).
+#
+#            Say, instead looking for 2 available GPUs automatically, one can pick up 2 GPUs manually by
+#            specifying the environment variable NV_GPU. For example:
+#
+#                  # initalize a container that will run xxx.py using GPU3,4
+#                  NV_GPU=3,4 ndrun python3 xxx.py
+#                  # the container will be closed automatically once the run is finished
+#
+#      -a, --alias [ALIAS]
+#            Alias of the docker container can be provided. This option takes effect only
+#            when the image runs at the daemoen mode (Jupyter Notebook).
+#
+#      --extra [EXTRA_OPTS]
+#            Extra docker options can be provided while activating the docker image.
+#            This is especially useful, if you'd like to do some extra port/volume mapping
+#            between the host machine and the docker container. For example:
+#
+#                  # Run the script (xxx.py). During the run, a volume at the host machine, i.e. 
+#                  # at [host_dir_path], will be mounted as a volume locating at [container_dir_path], 
+#                  # which is a path at the side of the docker container.
+#                  ndrun --extra "-v [host_dir_path]:[container_dir_path] -t mxnet python3 xxx.py
+#
+#      -v, --verbose
+#            Entering the verbose mode.
+#
+#      -h, --help
+#            Print a summary of the command-line options.
+#
+# Usage Example
+#
+#      print TensorFlow's version:
+#
+#            # creat a python script "check_tf_ver.py", which simply print out the version of TensorFlow
+#            printf "import tensorflow as tf \nprint('TF version=',tf.__version__)" > check_tf_ver.py
+#            # execute "check_tf_ver.py", using the latest TensorFlow image
+#            ndrun -t tensorflow python3 check_tf_ver.py
+#
+#      print MXNet's version:
+#
+#            # creat a python script "check_mxnet_ver.py", which simply print out the version of TensorFlow
+#            printf "import mxnet as mx \nprint('MXNet version=',mx.__version__)" > check_mxnet_ver.py
+#            # execute "check_mxnet_ver.py", using the latest MXNet image
+#            ndrun -t mx-cu9-dnn7-py3-18.03 python3 check_mxnet_ver.py
+#
+#       print info of the GPUs:
+#
+#             # The GPUs exposed to the running container can be monitored using nvidia-smi.
+#             ndrun -t tensorflow -n 2 nvidia-smi
+#
+##########################################################################################################
 #!/bin/bash
 #
-# This is a wrapper file that helps you to run your script using the docker images prepared by us.
-#
-# Example of Usage (print CNTK's version inside the running image):
-#   printf "import cntk \nprint('cntk version=',cntk.__version__)" > check_cntk_version.py
-#   ndrun -v -t cntk python3 check_cntk_version.py
-# 
-# Options available:
-#   -t: tag of the image (e.g. tf-cu9-dnn7-py3, cntk-cu8-dnn6-py3)
-#       or type of the image (e.g. tensorflow, cntk).
-#   -n: number of GPUs to be used.
-#   -v: verbose mode.
-#
-
+# Setting up default values.
 # By default, 1 GPU is to be used and the TensorFlow's docker image will be chosen. 
 NUM_GPUS=1                    # number of GPUs to be used
 IMG_TYPE='tensorflow'         # type of the image to be running
-
-# By default, port 8888 will be opened as Jupyter Notebook starts.
-# Remark: Jupyter Notebook starts only if there's no script given.
+EXTRA_OPTS=""                 # extra options while activating the docker image
 PORT=8888
+# By default, port 8888 will be opened as Jupyter Notebook starts.
+# Remark: Jupyter Notebook starts only if there's no given script.
 
-# The above variables can be changed via the optional arguments -t, -n and -p.
-while getopts 'vt:n:p:' FLAG; do
+# The above variables can be changed via the optional arguments -n, -t and -p.
+while getopts 'vht:n:p:a:-:' FLAG; do
   case "${FLAG}" in
     v) VERBOSE=true ;;
-    t) IMG_TYPE="${OPTARG}" ;;
+    h) HELP="true" ;;
+    t) IMG_TYPE="${OPTARG}" 
+       # Decide which tag of honghu/keras to be used later.
+       case "${IMG_TYPE}" in
+         "tensorflow") IMG_TAG='tf-latest' ;;
+               "cntk") IMG_TAG='cntk-latest';;
+              "mxnet") IMG_TAG='mx-latest' ;;
+             "theano") IMG_TAG='theano-latest' ;;
+                    *) IMG_TAG=${IMG_TYPE} ;;
+       esac ;;
     n) NUM_GPUS="${OPTARG}" ;;
     p) PORT="${OPTARG}" ;;
-    *) error "Unexpected option ${FLAG}" ;;
+    a) ALIAS="${OPTARG}" ;;
+    -)
+       case "${OPTARG}" in
+         verbose) VERBOSE=true ;;
+         help) HELP="true" ;;
+         type)
+             val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+             IMG_TYPE=${val} ;;
+         tag)
+             val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+             IMG_TAG=${val} ;;
+         num_gpus)
+             val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+             NUM_GPUS=${val} ;;  
+         port)
+             val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+             PORT=${val} ;;
+         alias)
+             val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+             ALIAS=${val} ;;
+         extra)
+             val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+             EXTRA_OPTS=${val} ;;
+         *)
+             if [ "$OPTERR" = 1 ] && [ "${optspec:0:1}" != ":" ]; then
+                 echo "Error! Unknown option --${OPTARG}" >&2
+                 exit 1
+             fi ;;
+       esac ;;
+    *) if [ "$OPTERR" = 1 ] && [ "${optspec:0:1}" != ":" ]; then
+                 echo "Error! Unknown option is given!" >&2
+                 exit 1
+             fi ;;
   esac
 done
 shift $((OPTIND-1))
 
-# Decide which tag of honghu/keras to be used later.
-case "${IMG_TYPE}" in
-  "tensorflow") IMG_TAG='tf-cu9-dnn7-py3-avx2-18.03' ;;
-        "cntk") IMG_TAG='cntk-cu9-dnn7-py3-18.03' ;;
-       "mxnet") IMG_TAG='mx-cu9-dnn7-py3-18.03' ;;
-      "theano") IMG_TAG='theano-cu9-dnn7-py3-18.03' ;;
-             *) IMG_TAG= ${IMG_TYPE} ;;
-esac
+# Display help.
+if [ "$HELP" == "true" ] ;then
+  bottom=$(grep -n "Usage Example" $0)
+  bottom_line_num=$((${bottom:0:2}))
+  up=$(grep -n "SYNOPSIS" $0)
+  up_line_num=$((${up:0:2}))
+
+  diff_lines=$((bottom_line_num-up_line_num+1))
+
+  info=$(cat /sbin/ndrun | head -n $(($bottom_line_num-1)))
+  info=$(echo "$info" | cut -b 2-)
+  info=$(echo "$info" | tail -n $(($diff_lines)))
+  echo "$info"
+  exit 0
+fi
 
 # Define colors for verbose/error messages
 CL_RED='\033[1;31m'   # color (light red)
@@ -77,33 +200,33 @@ fi
 INTEPRETER=$1
 
 # Check if the intepreter is given.
-if [ -z $INTEPRETER ];then
-  echo -e ${CL_GREEN} An intepreter such as python/python3, is not given.${NC}
+if [ -z "$INTEPRETER" ];then
+  echo -e ${CL_GREEN}An intepreter such as python/python3, is not given.${NC}
 fi
 
 # Script to be executed should be the second argument.
 EXE=$2
 if [ -z $EXE ];then
-  echo -e ${CL_GREEN} You did not provide me the script you wish to execute.${NC}
+  echo -e ${CL_GREEN}You did not provide me the script you wish to execute.${NC}
 fi
 
 # Extract optional arguments of the script.
 ARGS=$(echo $@ | cut -d' ' -f 3-)
 
-CONTAINER_VOL=/notebooks
+CONTAINER_VOL=/workspace
 HOST_VOL=${PWD}
 # $CONTAINER_VOL is a volume on the container side.
 # $HOST_VOL is a volume on the host side.
-# Later, we will mount $HOST_VOL to $CONTAINER_VOL in order to share data
-# between both of these sides.
+# Later, we will mount $HOST_VOL to $CONTAINER_VOL automatically such that data
+# can be shared between both sides.
 
 # Output some additional info if verbose is on.
 CL='\033[1;32m'   # with color
 NC='\033[0m'      # with no color
 if [ "$VERBOSE" == "true" ] ;then
-  echo -e ${CL_GREEN}Intepreter="\t" $INTEPRETER${NC}
-  echo -e ${CL_GREEN}Script to run="\t" $EXE${NC}
-  echo -e ${CL_GREEN}Script Args="\t" $ARGS${NC}
+  # echo -e ${CL_GREEN}INTEPRETER="\t" $INTEPRETER${NC}
+  # echo -e ${CL_GREEN}EXE="\t" $EXE${NC}
+  # echo -e ${CL_GREEN}ARGS="\t" $ARGS${NC}
   echo -e ${CL_GREEN}'"'${HOST_VOL}'"'' (on the side of Host) will be mounted to ''"'${CONTAINER_VOL}'"'' (on the side of Container).'${NC}
 fi
 
@@ -184,15 +307,25 @@ elif [ "${VERBOSE}" == "true" ] ;then
 fi
 
 # Start the Jupyter Notebook if no script is passed for running.
-if [ -z ${INTEPRETER} ] && [ -z ${EXE} ] ;then
+if [ -z "${INTEPRETER}" ] && [ -z "${EXE}" ] ;then
   echo -e ${CL_GREEN}Starting Jupyter Notebook...${NC}
   echo -e NV_GPU="${NV_GPU}"${NC}
-  container_id=$(nvidia-docker run -it \
+
+  if [ -z "${ALIAS}" ] ;then
+    container_id=$(nvidia-docker run -it \
                     -v ${HOST_VOL}:${CONTAINER_VOL} \
                     -p ${PORT}:8888 \
-                    -d \
-                    --rm \
-                    honghu/keras:${IMG_TAG} )
+                    ${EXTRA_OPTS} \
+                    -d honghu/keras:${IMG_TAG} )
+  else
+    container_id=$(nvidia-docker run -it \
+                    --name ${ALIAS} \
+                    -v ${HOST_VOL}:${CONTAINER_VOL} \
+                    -p ${PORT}:8888 \
+                    ${EXTRA_OPTS} \
+                    -d honghu/keras:${IMG_TAG} )
+  fi
+
   if [ "$?" == "0" ] ;then
     container_id=$(echo ${container_id} | head -n 1 | cut -c1-12)
 
@@ -212,33 +345,30 @@ if [ -z ${INTEPRETER} ] && [ -z ${EXE} ] ;then
     # Tell the user how to connect to the Jupyter Notebook via the given token.
     echo -e ${NC} '*' To use Jupyter Notebook, open a browser and connect to the following address:${NC}
     echo -e ${CL_BLUE} "  http://${my_ip}:${PORT}/?token=${notebook_token}"${NC}
-    if [ "${my_ip}" == "localhost" ] ;then
-      echo -e ${NC} ' ' Replace '"'localhost'"' to the IP address \
-                    that is visible to other computers, if you are not coming from localhost.${NC}
-    fi
-    echo -e ${NC} '*' To stop and remove this docker daemon, type:${NC}
-    echo -e ${CL_BLUE} "  docker stop ${container_id}"${NC}
+    echo -e ${NC} '*' To stop and remove this daemon container, type:${NC}
+    echo -e ${CL_BLUE} "  docker stop ${container_id} && docker rm ${container_id}"${NC}
   else
-    echo -e ${CL_RED} An error occured. It"'"s likely that the port you have specified is already in use.${NC}
-    echo -e ${CL_RED} Use the option -p to use another port.${NC}
+    echo -e ${CL_RED}An error has occured.
     exit 1
   fi
 
-# Or, if a script is passed for running, run it.
-# Notice that, in order to use CNTK's docker image, we'll have to
-# switch on CNTK's virtual environment.
-elif [[ "${IMG_TAG}" =~ ^cntk.* ]] ;then
-
-  SRC_CNTK="source /cntk/activate-cntk"
+# Or, if a command is given, execute it.
+elif [ -z $EXE ] ;then
+  CMD=${INTEPRETER}
+  echo -e ${CL_GREEN}Executing the given command...${NC}
   nvidia-docker run -it \
                     -v ${HOST_VOL}:${CONTAINER_VOL} \
                     --rm \
+                    ${EXTRA_OPTS} \
                     honghu/keras:${IMG_TAG} \
-                    -c "${SRC_CNTK} && ${INTEPRETER} ${CONTAINER_VOL}/${EXE} $ARGS"
+                    ${CMD}
+
+# Or, if a script is passed for running, run it.
 else
   nvidia-docker run -it \
                     -v ${HOST_VOL}:${CONTAINER_VOL} \
                     --rm \
+                    ${EXTRA_OPTS} \
                     honghu/keras:${IMG_TAG} \
                     ${INTEPRETER} ${CONTAINER_VOL}/${EXE} $ARGS
 fi
